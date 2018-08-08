298ffca099cd393ad68e4bd08b3ed85caf6bfacd
Aryan Faghihi <aryanfaghihi@gmail.com>
2018-08-08 21:29:46 +1000

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
+import PiesListContainer from "./components/PiesListContainer";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              🥧 Pie of the Day
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <PiesListContainer />
       </div>
     );
   }
---
+++ src/components/PiesListContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+class PiesListContainer extends Component {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default PiesListContainer;
